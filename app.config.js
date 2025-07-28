607182a40a150d248c6125e0c79a208f73706c67
calcricksonnn <calcrickson22@icloud.com>
2025-07-28 20:12:54 +0800

    Create app.config.js

---
+++ app.config.js
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+
+export default ({ config }) => ({
+  ...config,
+  extra: {
+    API_URL: process.env.API_URL,
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY
+  },
+  android: {
+    ...config.android,
+    config: {
+      googleMaps: {
+        apiKey: process.env.ANDROID_GOOGLE_MAPS_API_KEY
+      }
+    }
+  },
+  ios: {
+    ...config.ios,
+    config: {
+      googleMapsApiKey: process.env.IOS_GOOGLE_MAPS_API_KEY
+    }
+  }
+});
