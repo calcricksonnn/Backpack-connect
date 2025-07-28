6653a927d846547af00e728829f13d46fee50f91
calcricksonnn <calcrickson22@icloud.com>
2025-07-29 00:21:13 +0800

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,17 @@
-module.exports = {
-  presets: ['babel-preset-expo'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        alias: {
-          '@src': './src'
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'expo-router/babel',
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@src': './src'
+          }
         }
-      }
+      ]
     ]
-  ]
+  };
 };
