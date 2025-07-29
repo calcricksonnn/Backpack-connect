563a427467bab5b7857e205af858a13e433944bd
calcricksonnn <calcrickson22@icloud.com>
2025-07-29 08:27:54 +0800

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,8 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = getDefaultConfig(__dirname);
+const defaultConfig = getDefaultConfig(__dirname);
+
+// Ensure TypeScript files are resolvable
+defaultConfig.resolver.sourceExts.push('ts', 'tsx');
+
+module.exports = defaultConfig;
