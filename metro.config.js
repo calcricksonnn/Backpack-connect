19818df154c9df478aca91e361841f808e796d21
calcricksonnn <calcrickson22@icloud.com>
2025-07-29 11:09:00 +0800

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,21 +1,17 @@
+// metro.config.js
+
 const { getDefaultConfig } = require('expo/metro-config');
 
 module.exports = (async () => {
   const config = await getDefaultConfig(__dirname);
 
+  // Add TypeScript and SVG extensions
   config.resolver.sourceExts = [
-    'ts', 'tsx', 'js', 'jsx', 'json', 'cjs', 'svg'
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'cjs',
+    'svg'
   ];
-
-  config.transformer = {
-    babelTransformerPath: require.resolve('react-native-svg-transformer'),
-  };
-
-  config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== 'svg');
-
-  config.resolver.extraNodeModules = {
-    'expo-modules-core': require.resolve('expo-modules-core/build/index.js'),
-  };
-
-  return config;
-})();
